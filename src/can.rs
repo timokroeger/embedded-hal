ead97e88fe9b7a68b419a393647c7ca48621fd29
Timo Kröger <timokroeger93@gmail.com>
2020-05-22 15:33:20 +0200

    can: `FilteredReceiver` and associated traits

--- src/can.rs
+++ src/can.rs
@@ -87,3 +87,113 @@ pub trait Can {
     /// Returns a received frame if available.
     fn try_receive(&mut self) -> nb::Result<Self::Frame, Self::Error>;
 }
+
+/// Filter mask type.
+pub enum MaskType {
+    /// Each filter of the group has an individual mask.
+    Individual,
+
+    /// All filters of a group share a common filter mask.
+    Shared,
+}
+
+/// Remote frame filter behaviour description.
+pub enum RtrFilterBehavior {
+    /// The RTR bit is part of the filter and the mask.
+    ///
+    /// Both `Filter::allow_remote()` and `Filter::remote_only()` are implemented.
+    Configurable,
+
+    /// The RTR bit is part of the filter.
+    ///
+    /// `Filter::remote_only()` is implemented. `Filter::allow_remote()` has no
+    /// effect.
+    ConfigurableEitherDataOrRemote,
+
+    /// Both data and remote frames with a mathing identifier are accepted.
+    ///
+    /// `Filter::allow_remote()` nor `Filter::remote_only()` have an effect on the filter configuration.
+    RemoteAlwaysAllowed,
+
+    /// Only data remote frames with a mathing identifier are accepted.
+    ///
+    /// `Filter::allow_remote()` nor `Filter::remote_only()` have an effect on the filter configuration.
+    OnlyData,
+
+    /// Only data remote frames with a mathing identifier are accepted.
+    ///
+    /// `Filter::allow_remote()` nor `Filter::remote_only()` have an effect on the filter configuration.
+    OnlyRemote,
+}
+
+/// A filter group with its capabilities.
+pub trait FilterGroup {
+    /// Returns the number of consecutive filter with the same capability.
+    fn num_filters(&self) -> usize;
+
+    /// Returs `true` when extended 29bit identifiers are supported (in addition
+    /// to the standard 11bit identifiers).
+    fn extended(&self) -> bool;
+
+    /// Returns the filter mask type. `None` if no masks is supported.
+    fn mask(&self) -> Option<MaskType>;
+
+    /// Returs the filter behavior in regard to remote frames.
+    fn rtr(&self) -> RtrFilterBehavior;
+}
+
+/// CAN filter interface
+pub trait Filter {
+    /// Creates a filter that accepts all frames.
+    fn accept_all() -> Self;
+
+    /// Creates a filter that accepts frames with the specified identifier.
+    fn new(id: Id) -> Self;
+
+    /// Applies a mask to the filter.
+    ///
+    /// # Example
+    ///
+    ///    Filter ID:  0b100110111
+    ///    Mask:       0b000001111
+    ///    
+    ///    Receive ID: 0b100110011
+    ///                        \----> Not accepted (bit 3 did not match)
+    ///    
+    ///    Receive ID: 0b000000111 -> accepted
+    fn with_mask(&mut self, mask: u32) -> &mut Self;
+
+    /// Makes the filter acccept both data and remote frames.
+    ///
+    /// Sets the RTR bit in the filter mask.
+    /// Only available for filters with `RtrFilterBehavior::Configurable`.
+    fn allow_remote(&mut self) -> &mut Self;
+
+    /// Makes the filter acccept remote frames only.
+    ///
+    /// Sets the RTR bit in the filter and clears it in the mask (if available).
+    /// Only available for filters with `RtrFilterBehavior::Configurable` or
+    /// `RtrFilterBehavior::ConfigurableEitherDataOrRemote`.
+    fn remote_only(&mut self) -> &mut Self;
+}
+
+/// A CAN interface that is able to specify receive filters.
+pub trait FilteredReceiver: Can {
+    /// Associated filter type.
+    type Filter: Filter;
+
+    /// Associated filter group type.
+    type FilterGroup: FilterGroup;
+
+    /// Associated iterator type for the filter groups.
+    type FilterGroups: IntoIterator<Item = Self::FilterGroup>;
+
+    /// Returns the filter's groups.
+    fn filter_groups(&self) -> Self::FilterGroups;
+
+    /// Adds and enables a filter.
+    fn add_filter(&mut self, filter: &Self::Filter) -> Result<(), Self::Error>;
+
+    /// Clears all filters. No messages can be received anymore.
+    fn clear_filters(&mut self);
+}
